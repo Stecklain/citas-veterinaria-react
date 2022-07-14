dc003832176f3f932e38d440738df6c38bb6c831
Stecklain <juanmstecklain@gmail.com>
2022-07-13 22:10:50 -0300

    Segundo y ultimo commit

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+
+import Header from "./components/Header"
+import Formulario from "./components/Formulario"
+import ListadoPacientes from "./components/ListadoPacientes"
+import { useState, useEffect } from "react"
+
+function App() {
+  const [pacientes, setPacientes] = useState(JSON.parse(localStorage.getItem('pacientes')) ?? []);
+  const [paciente, setPaciente] = useState({});
+
+  useEffect(()=> {
+    localStorage.setItem('pacientes', JSON.stringify( pacientes ))
+  }, [pacientes]);
+
+  const altaPaciente = id => {
+    const pacientesActualizados = pacientes.filter(paciente => paciente.id !== id);
+    setPacientes(pacientesActualizados);
+  }
+
+  return (
+    <div className="container mx-auto mt-20">
+      <Header />
+
+      <div className="mt-12 md:flex">
+        <Formulario
+          pacientes={pacientes}
+          setPacientes={setPacientes}
+          paciente={paciente}
+          setPaciente={setPaciente}
+        />
+        <ListadoPacientes 
+          pacientes = {pacientes}
+          setPaciente={setPaciente}
+          altaPaciente={altaPaciente}
+        />
+      </div>
+    </div>
+  )
+}
+
+export default App
